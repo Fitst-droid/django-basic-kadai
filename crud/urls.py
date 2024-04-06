823168356509cb1febdf1ed602a265d75e18ccd6
Fitst-droid <own5mv07329633p@outlook.jp>
2024-04-06 17:24:41 +0900

    add detail

---
+++ crud/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+app_name ='crud'
+urlpatterns = [
+    path('', views.TopView.as_view(), name='top'),
+    path('crud/', views.ProductListView.as_view(), name="crud_list"),
+    path('crud/create/', views.ProductCreateView.as_view(), name="create"),
+    path('crud/detail/<int:pk>', views.ProductDetailView.as_view(), name="detail"),
+    # path('crud/edit/<int:pk>', views.ProductUpdateView.as_view(), name="edit"),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ crud/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.views.generic import  ListView, DetailView, TemplateView
+from django.views.generic.edit import  CreateView, UpdateView
+from django.urls import reverse_lazy
+from .models import Product
+
+class TopView(TemplateView):
+  template_name = 'top.html'
+
+class ProductCreateView(CreateView):
+  model = Product
+  fields = '__all__'
+  success_url = reverse_lazy('crud_list') 
+
+class ProductListView(ListView):
+  template_name = 'list.html'
+  model = Product
+  paginate_by = 4
+
+class ProductDetailView(DetailView):
+  model = Product
+  fileds = '__all__'
+  success_url = reverse_lazy('crud_list')
+
+# class ProductUpdateView(UpdateView):
+#   model = Product
+#   fileds = '__all__'
+#   template_name_suffix = '_updete_form.html'
+#   success_url = reverse_lazy('crud_list')
